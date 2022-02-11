23784bebcf6188566b3edc0613887e68923347cd
arkzoidal <github@arkzoidal.xyz>
2022-02-11 10:57:36 -0500

    update facts.d

---
+++ ansible/facts.d/sync.sh
@@ -0,0 +1,5 @@
+cp /home/vagrant/RACROC/ansible/facts.d/db1/nic.fact db1
+cp /home/vagrant/RACROC/ansible/facts.d/db1/virsh.fact db1
+cp /home/vagrant/RACROC/ansible/facts.d/db2/nic.fact db2
+cp /home/vagrant/RACROC/ansible/facts.d/db2/virsh.fact db2
+chown -R ansible:ansible *
