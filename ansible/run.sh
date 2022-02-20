2cf51d96b6bee22fcae07a2258271e5491d34f2a
arkzoidal <github@arkzoidal.xyz>
2022-02-20 12:56:08 -0500

    added new invocation

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,3 +1,5 @@
-/usr/bin/cp -f vagrant_ansible_inventory  inventory
-export ANSIBLE_HOME=/home/vagrant/RACROC/ansible
-ansible-playbook playbooks/oracle_GI_RAC.yml --extra-vars "hostgroup=rac19c01 pre_grid_setup=y"
+#ANSIBLE_HOME=/home/vagrant/RACROC/ansible ansible-playbook playbooks/oracle_GI_RAC.yml --extra-vars "hostgroup=rac21c01 pre_grid_setup=y"
+#ANSIBLE_HOME=/home/vagrant/RACROC/ansible ansible-playbook playbooks/oracle_DB_RAC.yml --extra-vars "hostgroup=rac21c01"
+#ANSIBLE_HOME=/home/vagrant/RACROC/ansible ansible-playbook playbooks/site.yml --extra-vars "hostgroup=rac21c01"
+
+ANSIBLE_HOME=/home/vagrant/RACROC/ansible ansible-playbook playbooks/oracle_CREATE_DB.yml --extra-vars "hostgroup=rac21c01"
