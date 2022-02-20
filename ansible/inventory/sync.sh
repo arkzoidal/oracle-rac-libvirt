9b3878688f3e65e06b9689b47b8026ae748f0fd6
arkzoidal <github@arkzoidal.xyz>
2022-02-20 14:13:05 -0500

    sync

--- ansible/inventory/sync.sh
+++
@@ -1,7 +0,0 @@
-cp /home/vagrant/RACROC/ansible/inventory/group_vars/rac19c01/passwords.yml	./group_vars/rac19c01/passwords.yml
-cp /home/vagrant/RACROC/ansible/inventory/group_vars/rac19c01/rac19c01.yml	./group_vars/rac19c01/rac19c01.yml
-cp /home/vagrant/RACROC/ansible/inventory/group_vars/all	./group_vars/all
-cp /home/vagrant/RACROC/ansible/inventory/host_vars/db1	./host_vars/db1
-cp /home/vagrant/RACROC/ansible/inventory/host_vars/db2	./host_vars/db2
-cp /home/vagrant/RACROC/ansible/inventory/vagrant_ansible_inventory	./vagrant_ansible_inventory
-chown ansible:ansible *
