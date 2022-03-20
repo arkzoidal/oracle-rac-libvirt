3ba238b481c3b321eccc87f07541462a7b9fd238
arkzoidal <github@arkzoidal.xyz>
2022-03-20 11:19:05 -0400

    deleted *old

--- ansible/roles/oracle-gi-install-role/tasks/old/check_ssa.sh
+++
@@ -1,2 +0,0 @@
-/home/grid/cvu/bin/cluvfy comp ssa -n db1,db2 -asm -asmdev "/dev/oracleasm/asm_rac19c01_data_001"
-/home/grid/cvu/bin/cluvfy comp ssa -n db1,db2 -asm -asmdev "/dev/sdg"
--- ansible/roles/oracle-gi-install-role/tasks/old/cvu_check_grid_prerequisites.sh
+++
@@ -1,1 +0,0 @@
-/u01/app/19.3.0.0/grid/runcluvfy.sh comp sys -n db1,db2 -p crs -r 19 -verbose
